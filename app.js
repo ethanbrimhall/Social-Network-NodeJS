1b3c5274b57cd56ed7bfd7fb74229d640fec7cc8
Ethan Brimhall <ethanbrimhall@Ethans-MBP.fios-router.home>
2019-01-16 14:09:02 -0500

    Redirects to login if not authenticated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
 
 // Init app
 const app = express();
@@ -9,9 +10,23 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Feed route
-app.get('/', (req, res) =>{
+app.get('/', ensureAuthenticated, (req, res) =>{
   res.render('index');
 });
+
+
+
+// Access Control
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect('/users/login');
+  }
+}
+
+let users = require('./routes/users');
+app.use('/users', users);
 
 app.listen(3000, () => {
   console.log('Server Started on port 3000');
